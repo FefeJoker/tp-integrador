4994d5553fb1d87d9486a888c2a249b536d5f7a1
Santiago Cobelli <cobellisantiago@gmail.com>
2019-06-04 14:36:10 -0300

    parte 2

---
+++ src/main/java/isi/died/tp/dominio/Insumo.java
@@ -0,0 +1,15 @@
+package isi.died.tp.dominio;
+
+public abstract class Insumo {
+
+   protected int id;
+   protected String descripcion;
+   protected String unidadDeMedida;
+   protected Integer costo;
+   protected Float stock;
+   protected Float peso; //en Kg
+   protected Boolean esRefrigerado;
+
+
+
+}
